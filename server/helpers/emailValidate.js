92fe96eec991ce6d8a7d0433139caee7001ec726
RAMAZAN DOGAN <doganrmzn40@gmail.com>
2024-02-10 15:51:42 +0300

    Fix email validation error

--- server/helpers/emailValidate.js
+++ server/helpers/emailValidate.js
@@ -1,13 +1,8 @@
-const validateEmail = (email) => {
-    // Geçerli e-posta formatını kontrol etmek için bir regex kullanıyoruz
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-    // Eğer e-posta formatı uygun değilse false döndürüyoruz
+const emailValidate = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;    
     if (!regex.test(email)) {
         return false;
     }
-
-    // E-posta domain'inin belirli bir listeye dahil olup olmadığını kontrol edebiliriz
     const allowedDomains = [
         'gmail.com',
         'yahoo.com',
@@ -18,16 +13,11 @@ const validateEmail = (email) => {
         'yandex.com',
         'mail.com',
     ];
-
     const domain = email.split('@')[1]; // E-posta adresinden domain'i alıyoruz
-
-    // Eğer e-posta domain'i izin verilenler listesinde değilse false döndürüyoruz
     if (!allowedDomains.includes(domain)) {
         return false;
     }
-
-    // Eğer e-posta hem geçerli bir formatta hem de izin verilen bir domain'e sahipse true döndürüyoruz
     return true;
 }
 
-module.exports = validateEmail;
+module.exports = {emailValidate};
