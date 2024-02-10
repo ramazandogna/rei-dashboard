fe03969decd595be1af28b8b017cfb1f9225e44e
RAMAZAN DOGAN <doganrmzn40@gmail.com>
2024-02-10 21:58:00 +0300

    corsOption and axios configiration

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -4,47 +4,57 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/users',
     name: 'Users',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Users.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Users.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/ui',
     name: 'Ui',
-    component: () => import(/* webpackChunkName: "home" */ '../views/UI.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/UI.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/tables',
     name: 'Tables',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Tables.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Tables.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/forms',
     name: 'Forms',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Forms.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Auth/Login.vue')
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Auth/Register.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Forms.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/modals',
     name: 'Modals',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Modals.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Modals.vue'),
+    meta: { requiresUnauth: true }
   },
   {
     path: '/errors',
     name: 'Errors',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Errors.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Errors.vue'),
+    meta: { requiresUnauth: true }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Auth/Login.vue'),
+
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Auth/Register.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/:pathMatch(.*)*',
@@ -55,6 +65,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, _from, next) => {
+  if (to.meta.requiresAuth && !localStorage.getItem('token')) {
+    next('/')
+  } else if (to.meta.requiresUnauth && localStorage.getItem('token')) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
