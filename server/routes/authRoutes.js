61b7d32abc3efad8b010e4ede59998df9f2254e8
RAMAZAN DOGAN <doganrmzn40@gmail.com>
2024-02-10 13:57:58 +0300

    auth routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const {
+    test
+} = require('../controllers/authController')
+
+
+//middleware
+router.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+)
+
+//routes
+router.get('/', test)
+
+
+module.exports = router;
